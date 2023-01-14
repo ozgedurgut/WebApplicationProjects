653ba26ec8aadfbd898f60b728f1359dd9541918
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-01-14 14:28:28 +0300

    create MVC frame

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace WebApplication.Controllers
     // Controller: In order to make a class request and return response, that is, to make a controller, we need to derive that class from the controller class
     public class HomeController : Controller
     {
+        // Controller sınıfları içerisinde istekleri karşılayan metotlara action metot denir.
         public IActionResult Index()
         {
             return View();
--- WebApplication/Controllers/ProductController.cs
+++ WebApplication/Controllers/ProductController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        public IActionResult GetProducts()
         {
-            return View();
+            Product product = new Product();
+
+            ViewResult result = View(); // ilgili action ismiyle birebir aynı olan viewı tetikler.
+            return result;
+            // ViewResult result = View("viewdosyasınınadı"); 
+            // ya yukarıdaki gibi ya da sadece alttaki satırı kullanarak aynı işlemi yapabiliriz.
+
+            //return View();
+            // view fonksiyonu bu actiona ait view(.cshtml) dosyasını tetikleyecektir.
         }
     }
 }
